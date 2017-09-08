40334f53ffbd231e95ebfb637b3042b5cc45ec17
Dave Goodman <davegoodman20@gmail.com>
2017-09-08 11:13:38 -0400

    mock challenge

--- author.rb
+++ author.rb
@@ -1,1 +1,29 @@
+class Author
+  attr_accessor :name, :books
 
+  @@authors = []
+
+  def initialize(name)
+    @name = name
+    @books = []
+    @@authors << self
+  end
+
+  def self.all
+    @@authors
+  end
+
+  def books
+    @books
+  end
+
+  def total_words
+  end
+
+  def write_book(title, author, word_count)
+    book = Book.new(title, self, word_count)
+    book.word_count += word_count
+    @books << book
+  end
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,25 @@
+class Book
+    attr_accessor :name, :author, :word_count, :title
 
+    @@books = []
+
+    def initialize(title, author, word_count)
+      @word_count = word_count
+      @title = title
+      @author = author
+      @@books << self
+    end
+
+    def self.most_words
+      #needs to iterate through all of the books, collecting book.word_count
+    end
+
+    def word_count
+      @word_count
+    end
+
+    def self.all
+      @@books
+    end
+
+  end
